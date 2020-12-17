1127cc7de068f11bbf39dd53ee64d032c0bfb2af
fat <fatoche49@YAHOO.fr>
2020-12-17 12:22:55 +0100

    gestion category

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _UnitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        #endregion
+    }
+}
